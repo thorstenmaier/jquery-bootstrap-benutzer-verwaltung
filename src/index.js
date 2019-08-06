e270e3ead0f6b641803d3af3f3ab471dd81248ac
Thorsten Maier <thorsten.maier@oio.de>
2019-08-06 17:08:38 +0200

    Finaler Stand Workshop Tag 1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import $ from "jquery";
 
-$(function() {
-  $("#vorname").keydown(function(event) {
+const BENUTZERS_URL = "http://localhost:8080/benutzers";
+
+$(function () {
+  $("#vorname").keydown(function (event) {
     if (event.which == 13) {
       $("#nachname").val("").focus();
       event.preventDefault();
@@ -9,7 +11,7 @@ $(function() {
     }
   });
 
-  $("#nachname").keydown(function(event) {
+  $("#nachname").keydown(function (event) {
     if (event.which == 13) {
       neuenBenutzerAnlegen();
       event.preventDefault();
@@ -17,21 +19,42 @@ $(function() {
     }
   });
 
-  $("#save").click(function(event) {
+  $("#save").click(function (event) {
     neuenBenutzerAnlegen();
   });
 
-  console.log("Anwendung wurde gestartet");
+  reloadList();
 });
 
 function neuenBenutzerAnlegen() {
   let benutzer = {};
   benutzer.vorname = $("#vorname").val();
   benutzer.nachname = $("#nachname").val();
-  console.log(benutzer);
+
+  $.ajax(BENUTZERS_URL, {
+    data: JSON.stringify(benutzer),
+    contentType: 'application/json',
+    type: 'POST',
+    success: function() {
+      reloadList();
+    }
+  });
 
   $("#vorname").val("").focus();
   $("#nachname").val("");
-  
-  $("#benutzerliste tbody").append("<tr><td>" + benutzer.vorname + "</td><td>" + benutzer.nachname +"</td></tr>");
+}
+
+function reloadList() {
+  $.ajax({
+    dataType: "json",
+    url: BENUTZERS_URL,
+    success: function(data) {
+      $("#benutzerliste tbody").empty();
+      let benutzers = data["_embedded"]["benutzers"];
+      for (let idx in benutzers) {
+        let benutzer = benutzers[idx];
+        $("#benutzerliste tbody").append("<tr><td>" + benutzer.vorname + "</td><td>" + benutzer.nachname + "</td></tr>");
+      }
+    }
+  });
 }
