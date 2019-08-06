33f44a5cf442772d8ce77e328038a6d078acaf85
Thorsten Maier <thorsten.maier@oio.de>
2019-08-06 15:30:47 +0200

    Benutzer anlegen Formular clientseitig fertig

--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
-document.getElementById("firstDiv").innerHTML = "Hallo ADF+";
-console.log("Anwendung wurde gestartet");
+import $ from "jquery";
+
+$(function() {
+  $("#vorname").keydown(function(event) {
+    if (event.which == 13) {
+      $("#nachname").val("").focus();
+      event.preventDefault();
+      return false;
+    }
+  });
+
+  $("#nachname").keydown(function(event) {
+    if (event.which == 13) {
+      neuenBenutzerAnlegen();
+      event.preventDefault();
+      return false;
+    }
+  });
+
+  $("#save").click(function(event) {
+    neuenBenutzerAnlegen();
+  });
+
+  console.log("Anwendung wurde gestartet");
+});
+
+function neuenBenutzerAnlegen() {
+  let benutzer = {};
+  benutzer.vorname = $("#vorname").val();
+  benutzer.nachname = $("#nachname").val();
+  console.log(benutzer);
+
+  $("#vorname").val("").focus();
+  $("#nachname").val("");
+  
+  $("#benutzerliste tbody").append("<tr><td>" + benutzer.vorname + "</td><td>" + benutzer.nachname +"</td></tr>");
+}
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  devServer: {
+    contentBase: './dist',
+    hot: false
+  },
+  devtool: 'inline-source-map'
+};
