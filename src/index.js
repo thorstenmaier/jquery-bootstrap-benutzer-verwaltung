1d758ebd94e5e291aed8c32579ef8498d5b7efa7
Thorsten Maier <Thorsten.Maier@oio.de>
2021-03-23 09:20:52 +0100

    Update deprecated jquery functions

--- src/index.js
+++ src/index.js
@@ -3,23 +3,23 @@ import $ from "jquery";
 const BENUTZERS_URL = "http://localhost:8080/benutzers";
 
 $(function () {
-  $("#vorname").keydown(function (event) {
-    if (event.which == 13) {
-      $("#nachname").val("").focus();
+  $("#vorname").on('keydown', function (event) {
+    if (event.key == 'Enter') {
+      $("#nachname").val("").trigger('focus');
       event.preventDefault();
       return false;
     }
   });
 
-  $("#nachname").keydown(function (event) {
-    if (event.which == 13) {
+  $("#nachname").on('keydown', function (event) {
+    if (event.key == 'Enter') {
       neuenBenutzerAnlegen();
       event.preventDefault();
       return false;
     }
   });
 
-  $("#save").click(function (event) {
+  $("#save").on('click', function (event) {
     neuenBenutzerAnlegen();
   });
 
@@ -35,12 +35,12 @@ function neuenBenutzerAnlegen() {
     data: JSON.stringify(benutzer),
     contentType: 'application/json',
     type: 'POST',
-    success: function() {
+    success: function () {
       reloadList();
     }
   });
 
-  $("#vorname").val("").focus();
+  $("#vorname").val("").trigger('focus');
   $("#nachname").val("");
 }
 
@@ -48,12 +48,23 @@ function reloadList() {
   $.ajax({
     dataType: "json",
     url: BENUTZERS_URL,
-    success: function(data) {
+    success: function (data) {
       $("#benutzerliste tbody").empty();
       let benutzers = data["_embedded"]["benutzers"];
       for (let idx in benutzers) {
         let benutzer = benutzers[idx];
-        $("#benutzerliste tbody").append("<tr><td>" + benutzer.vorname + "</td><td>" + benutzer.nachname + "</td></tr>");
+        $("#benutzerliste tbody").append("<tr>"
+          + "<td>"
+          + benutzer.id
+          + "</td>"
+          + "<td>"
+          + benutzer.vorname
+          + "</td>"
+          + "<td>"
+          + benutzer.nachname
+          + "</td>"
+          + "<td><button>Löschen</button></td>"
+          + "</tr>");
       }
     }
   });
